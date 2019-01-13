5ba8ae4c59a72813ce4338c983cd087cacb25b02
Tomasz Pietruszka <tomek.pietruszka@gmail.com>
2019-01-13 17:29:55 +0100

    non-ascii char removed from code. Caused display bugs

--- ulmfit/postprocess_wikitext.py
+++ ulmfit/postprocess_wikitext.py
@@ -53,7 +53,7 @@ def limit_vocab(unk_path, vocab):
             tokens = [''] + tokens
             line = ' '.join(tokens)
             f_out.write(line)
-    print(f'{unk_path.name}. # of tokens: {total_num_tokens}')
+    print(f'{unk_path.name}. # of tokens: {total_num_tokens}')
     temp_file_path.replace(unk_path)
 
 
@@ -100,6 +100,7 @@ def postprocess_wikitext(path, lang):
     for split in splits:
         unk_path = dest_path / f'{lang}.wiki.{split}.tokens'
         limit_vocab(unk_path, vocab)
+
 
 if __name__ == '__main__':
     fire.Fire(postprocess_wikitext)
