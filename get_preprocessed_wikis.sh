22cb8b1660eed807d311a269ec9df3b216edfd71
Marcin <marcin.kardas@gmail.com>
2019-02-15 00:00:20 +0100

    Download pretrained models

--- get_preprocessed_wikis.sh
+++ get_preprocessed_wikis.sh
@@ -7,3 +7,12 @@ mkdir -p "${OUTDIR}"
 unzip "${ZIPNAME}" -d "${OUTDIR}"
 
 for archive in "${OUTDIR}"/??-100.tar.gz; do tar xvf "${archive}" -C "${OUTDIR}" && rm "${archive}"; done
+
+#optionally download models
+MODELS="pretrained_lm_models.zip"
+read -r -p "Download pretrained lm models? [y/N] " response
+if [[ "$response" =~ ^[yY]$ ]]
+then
+    wget -nc 'https://www.dropbox.com/sh/srfwvur6orq0cre/AAABRFdrCNHmbpf4nNcMiJwJa/models/data/wiki?dl=1' -O "${MODELS}"
+    unzip "${MODELS}" -d "${OUTDIR}"
+fi
